7ba6a8759d58e196c6338ca250be0889115b2aa5
shubham kumar <clboy768@gmail.com>
2025-12-04 16:03:07 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,86 @@
+// let x = 10;
+// let y= 4;
+
+// console.log(x+y);
+
+// Basic of Javascript
+
+// Data Type  --> There are two type od Data type
+// 1)Primitive   -->that can store only single value
+// 2)Non Primitive  --> can store collection of Data
+
+// Difference between let var and const -->
+
+// Let -->a)  redeclaration is not possible
+// let x = 10;
+// let x = 5;
+// console.log(x);
+
+// b) reassign is possible in case of let
+// let x = 10;
+//     x = 6;
+//     console.log(x)
+
+// 2) Var   -->
+// a)redeclaration-->redecleration is possible
+    // var a = 10;
+    // var a = 7;
+    // console.log(a)
+
+// b) Reassigning-->
+    // var a = 10;
+    //     a= 9;
+    //     console.log(a)
+
+// 3) Const -->
+    // a)redecleration -->
+    // const a = 10;
+    // const a = 7;
+    // console.log(a)
+
+    // b)Reassign -->
+    // const a= 10;
+    //     a = 3;
+    //     console.log(a)
+
+
+    // NonPrimitive Data type
+
+    // a)Array --> 
+
+    // let name = 'abc';
+    // let email = 'abc@gmail.com';
+    // let course = 'fullstack';
+    // let age = 22;
+
+
+    // let arr = ['abc', 'abc@gmail.com', 'fullstack', 22]
+    // console.log(arr)
+
+    // Object --> object is also a collection of data.. and each data is stored in key and value form
+
+    // let data = {
+    //     name : 'abc',
+    //     age: 34,
+    //     course:"fullstack",
+    //     email:"abc@gmail.com",
+    //     languages:['hindi', 'english','spanish'],
+    //     details:{
+    //         houseNo:11,
+    //         state:"UP",
+    //         city:"Lucknow"
+    //     }
+    // }
+
+
+    // let details = [
+    //     10,
+    //     'hello',
+    //     {name:"one",age:23},
+    //     'frontend'
+    // ]
+
+    
+
+
+
