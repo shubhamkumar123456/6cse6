0afe46d1a661ef9d9c1538a4e4c6e8956cf10ed0
shubham kumar <clboy768@gmail.com>
2025-12-27 14:13:03 +0530

    added array

--- Question.js
+++ Question.js
@@ -1,3 +1,19 @@
-let text = "hello how are you" // output --< hellohowareyou
-let ans = text.replace(/ /g,'')
-console.log(ans)
+// let text = "hello how are you" // output --< hellohowareyou
+// let ans = text.replace(/ /g,'')
+// console.log(ans)
+
+
+
+
+// let text = "hello how are you"  //output --> "you are how hello"
+// let arr = text.split(' ')
+// arr.reverse()
+// console.log(arr)  //[ 'you', 'are', 'how', 'hello' ]
+
+// let ans = arr.join(' ');
+// console.log(ans)  //you are how hello
+
+
+
+
+
