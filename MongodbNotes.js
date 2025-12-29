4f436049b4ab8af4937a60b4851c0fa4798d8770
shubham kumar <clboy768@gmail.com>
2025-12-29 13:13:03 +0530

    updated mongodb commands

---
+++ MongodbNotes.js
@@ -0,0 +1,20 @@
+//Mongodb --> it is a popular open source No sql database . instead of using  tables and rows like traditional database,it uses JSON like object to stroe data
+
+// Database --> collection --> document(data)
+
+
+// Commands -->
+
+//1) TO view all database --> show dbs
+//2) create a new Database or use any existing Database --> use DatabaseName
+//3) create a collection --> db.createCollection('collectionName)
+
+// Insert Data inside collection --> 
+    // a) insert single Data --> db.collectionName.insertOne({key:value})
+    // example --> db.users.insertOne({name:"one",email:"one@gmail.com"})
+
+    //b) insert multiple Data --> db.collectionName.insertMany( [ {} , {}] )
+
+    
+
+// 4) find all data present inside a collection --> db.collectionName.find()
